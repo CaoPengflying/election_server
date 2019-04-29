91908bf946cb6694910a1d4ec0b03d93e901d551
CaoPengFlying <18679229291@163.com>
2019-04-29 17:49:10 +0800

    创建helloController

---
+++ src/main/java/com/zzc/election_server/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.zzc.election_server.controller;
+
+import com.zzc.election_server.common.ErrorConstant;
+import com.zzc.election_server.common.Result;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author caopengflying
+ * @time 2019/4/29 17:46
+ */
+@RestController
+@RequestMapping(value = "hello")
+public class HelloController {
+    @RequestMapping(value = "hello")
+    public Result hello(){
+        System.out.println("hello ");
+        return ErrorConstant.getSuccessResult("");
+    }
+}
