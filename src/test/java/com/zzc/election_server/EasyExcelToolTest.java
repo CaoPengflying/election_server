e4db3926fa16b223164ef2df8fcaaae82464a7a5
CaoPengFlying <18679229291@163.com>
2019-08-24 17:14:00 +0800

    增加easyExcel读单元测试

--- src/test/java/com/zzc/election_server/EasyExcelToolTest.java
+++ src/test/java/com/zzc/election_server/EasyExcelToolTest.java
@@ -2,31 +2,23 @@ package com.zzc.election_server;
 
 import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.excel.ExcelWriter;
-import com.alibaba.excel.metadata.Font;
 import com.alibaba.excel.metadata.Sheet;
-import com.alibaba.excel.metadata.Table;
-import com.alibaba.excel.metadata.TableStyle;
 import com.alibaba.excel.support.ExcelTypeEnum;
-import com.google.common.collect.Lists;
 import com.zzc.election_server.mapper.StudentMapper;
 import com.zzc.election_server.model.ExcelStudent;
 import com.zzc.election_server.model.Student;
 import com.zzc.election_server.model.WriteModel;
 import com.zzc.election_server.util.FileUtil;
 import com.zzc.election_server.util.ModelTransformUtils;
-import org.apache.poi.ss.usermodel.IndexedColors;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.BeanUtils;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.annotation.Resource;
 import java.io.*;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static com.zzc.election_server.util.DataUtil.*;
 
@@ -76,4 +68,19 @@ public class EasyExcelToolTest {
 
     }
 
+    @Test
+    public void testReadV2007() throws IOException {
+        InputStream inputStream = FileUtil.getResourcesFileInputStream("tb_student.xlsx");
+        List<Object> read = EasyExcelFactory.read(inputStream, new Sheet(1, 0, ExcelStudent.class));
+        inputStream.close();
+        print(read);
+
+    }
+    public void print(List<Object> datas){
+        int i=0;
+        for (Object ob:datas) {
+            System.out.println(i++);
+            System.out.println(ob);
+        }
+    }
 }
