6ba44ca97a6e130a1051fcdde3791d60e5d3dbbc
CaoPengFlying <18679229291@163.com>
2019-08-24 16:33:32 +0800

    增加easyExcel模块使用

--- src/main/java/com/zzc/election_server/controller/HelloController.java
+++ src/main/java/com/zzc/election_server/controller/HelloController.java
@@ -6,6 +6,7 @@ import com.zzc.election_server.model.Student;
 import com.zzc.election_server.service.HelloService;
 import com.zzc.election_server.service.HelloService2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,26 +19,33 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping(value = "hello")
 public class HelloController {
-    @RequestMapping(value = "hello")
-    public Result hello(){
-        System.out.println("hello ");
-        System.out.println("dev  update again");
-        return ErrorConstant.getSuccessResult("hello docker");
-    }
+
+    @Value("${config:1000}")
+    private String config;
     @Autowired
     private HelloService helloService;
+    @Autowired
+    private HelloService2 helloService2;
+
+    @RequestMapping(value = "hello")
+    public Result hello() {
+        System.out.println("hello ");
+        System.out.println("dev  update again");
+        System.out.println("config:" + config);
+        return ErrorConstant.getSuccessResult("hello docker");
+    }
+
     @RequestMapping(value = "testTransactional")
-    public Result testTransactional(){
+    public Result testTransactional() {
         Student student = new Student();
         student.setStudentId(1);
         student.setStudentName("test003");
         helloService.updateStudent(student);
         return ErrorConstant.getSuccessResult("");
     }
-    @Autowired
-    private HelloService2 helloService2;
+
     @RequestMapping(value = "testTransactional2")
-    public Result testTransactional2(){
+    public Result testTransactional2() {
         Student student = new Student();
         student.setStudentId(1);
         student.setStudentName("test001");
